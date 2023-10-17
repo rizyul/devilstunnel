fe0f0d783a6ac0b53ba6cfe0c7d78c03f3f148fe
rizyul <124092394+rizyul@users.noreply.github.com>
2023-10-17 13:38:52 +0700

    Update ub20.sh

--- ub20.sh
+++ ub20.sh
@@ -163,7 +163,6 @@ wget -q -O /root/cf.sh https://raw.githubusercontent.com/rizyul/deviltunnel/main
 rm -f /root/cf.sh
 rm -rf ub20.sh
 clear
-    fi
  # "Installed slowdns"
     wget -q -O /etc/nameserver "https://github.com/rizyul/devilstunnel/main/slowdns/nameserver" && bash /etc/nameserver >/dev/null 2>&1	
 }
